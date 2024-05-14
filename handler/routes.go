71f259456d9b729f01fab8d509690e2edbeaf85a
dany ismail <dany.perdana@smartfren.com>
2024-05-14 15:59:18 +0700

    feat: add swagger , upgrade echo

--- handler/routes.go
+++ handler/routes.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/labstack/echo"
+	"github.com/labstack/echo/v4"
 	"gopkg.in/go-playground/validator.v9"
 )
 
--- structs/request.go
+++ structs/request.go
@@ -1,15 +1,15 @@
 package structs
 
-import "github.com/labstack/echo"
+import "github.com/labstack/echo/v4"
 
 type PageView struct {
-	StartDt string `json:"startDt"`
-	EndDt   string `json:"endDt"`
-	Page    int    `json:"page"`
-	View    int    `json:"view"`
-	Mdn     string `json:"mdn"`
-	Status  int    `json:"status"`
-	Shift   string `json:"shift"`
+	StartDt string `json:"startDt" example:"2024-05-11"`
+	EndDt   string `json:"endDt" example:"2024-05-11"`
+	Page    int    `json:"page" example:"1"`
+	View    int    `json:"view" example:"10"`
+	Mdn     string `json:"mdn" example:"081284088408"`
+	Status  int    `json:"status" example:"20"`
+	Shift   string `json:"shift" example:"sore"`
 }
 
 func (r *PageView) Binding(c echo.Context) error {
