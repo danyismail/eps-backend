f613e53f47ad280b2e8006f5a925cf7f76ee0b16
dany ismail <dany.perdana@smartfren.com>
2024-06-26 10:33:00 +0700

    refactor (login response) add field expires_at

--- handler/user.go
+++ handler/user.go
@@ -195,7 +195,7 @@ func (h *Handler) Login(c echo.Context) error {
 		})
 	}
 
-	token, err := utils.GenerateJWT(os.Getenv("JWT_SECRET"), *user, 15)
+	token, exp, err := utils.GenerateJWT(os.Getenv("JWT_SECRET"), *user, 15)
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, structs.SimpleCommonResponse{
 			StatusCode: http.StatusForbidden,
@@ -207,9 +207,10 @@ func (h *Handler) Login(c echo.Context) error {
 		StatusCode: http.StatusOK,
 		Message:    "success",
 		Data: structs.LoginResponse{
-			Username: user.Username,
-			Role:     user.Role,
-			Token:    token,
+			Username:  user.Username,
+			Role:      user.Role,
+			Token:     token,
+			ExpiresAt: exp,
 		},
 	})
 }
