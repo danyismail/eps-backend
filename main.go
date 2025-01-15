ad44351d9554ee9f211b56bcc039d6716c73e22b
dany <dany_arie@yahoo.com>
2025-01-15 17:22:05 +0700

    trigger deploy

--- main.go
+++ main.go
@@ -11,8 +11,6 @@ import (
 )
 
 func main() {
-	//set output log file...
-
 	file, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatal("Failed to open log file...:", err)
