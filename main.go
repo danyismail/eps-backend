2ac870de05cc5f39e5e91f4442a9ab4cabff5bd4
dany <dany_arie@yahoo.com>
2025-06-10 16:41:44 +0700

    trigger deploy

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 func main() {
 	file, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
-		log.Fatal("Failed to open log file...:", err)
+		log.Fatal("Failed to open log file..:", err)
 	}
 	err = godotenv.Load()
 	if err != nil {
