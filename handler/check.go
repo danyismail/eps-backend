00c145ff298b2329f3a73fb129c4faf0b48e5ee0
dany.perdana <dany.ismail.perdana@gmail.com>
2026-01-11 14:23:19 +0700

    docs(handler): update health check response message to include version

--- handler/check.go
+++ handler/check.go
@@ -10,7 +10,7 @@ import (
 func (h *Handler) HealthCheck(c echo.Context) error {
 	h.e.Logger.Info("::HealthCheck Started::")
 	return c.JSON(http.StatusOK, structs.CommonResponse{
-		Data:       "api is alive",
+		Data:       "api is alive v1.0.1",
 		StatusCode: http.StatusOK,
 		Message:    "success",
 	})
